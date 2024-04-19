081784606867c17e473564c04b8cdee7a33e4324
Harsh <hsharma@mun.ca>
2024-04-19 19:05:21 -0230

    Fixed problem with import

--- js/Util/ResourceFile.js
+++ js/Util/ResourceFile.js
@@ -7,15 +7,15 @@
  *
  */
 export let resourceFiles = [
-	{ name: "tom", url: "/Models/cat/tom.glb" }, // Tom the cat model
-	{ name: "jerry", url: "/Models/mouse/jerry.glb" }, // Jerry the mouse model
-	{ name: "spike", url: "/Models/Dog/spike.glb" }, // Spike the dog model
-	{ name: "jerryFriend1", url: "/Models/mouse/jerryFriendOne.glb" }, // Friend 1 of Jerry
-	{ name: "jerryFriend2", url: "/Models/mouse/jerryFriendTwo.glb" }, // Friend 2 of Jerry
-	{ name: "jerryFriend3", url: "/Models/mouse/jerryFriendThree.glb" }, // Friend 3 of Jerry
-	{ name: "building1", url: "/Models/building/large_building_0.glb" }, // Large building model
-	{ name: "building2", url: "/Models/building/large_building_1.glb" }, // Large building model
-	{ name: "building3", url: "/Models/building/large_building_2.glb" }, // Large building model
-	{ name: "building4", url: "/Models/building/large_building_3.glb" }, // Large building model
-	{ name: "building5", url: "/Models/building/large_building_4.glb" }, // Large building model
+	{ name: "tom", url: "./Models/cat/tom.glb" }, // Tom the cat model
+	{ name: "jerry", url: "./Models/mouse/jerry.glb" }, // Jerry the mouse model
+	{ name: "spike", url: "./Models/Dog/spike.glb" }, // Spike the dog model
+	{ name: "jerryFriend1", url: "./Models/mouse/jerryFriendOne.glb" }, // Friend 1 of Jerry
+	{ name: "jerryFriend2", url: "./Models/mouse/jerryFriendTwo.glb" }, // Friend 2 of Jerry
+	{ name: "jerryFriend3", url: "./Models/mouse/jerryFriendThree.glb" }, // Friend 3 of Jerry
+	{ name: "building1", url: "./Models/building/large_building_0.glb" }, // Large building model
+	{ name: "building2", url: "./Models/building/large_building_1.glb" }, // Large building model
+	{ name: "building3", url: "./Models/building/large_building_2.glb" }, // Large building model
+	{ name: "building4", url: "./Models/building/large_building_3.glb" }, // Large building model
+	{ name: "building5", url: "./Models/building/large_building_4.glb" }, // Large building model
 ];
